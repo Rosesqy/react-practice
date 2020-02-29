5ca14b3a4580c6fbae50185aca167b45ab466220
Qingyang Shi <Rosesqy@users.noreply.github.com>
2020-02-29 18:59:33 -0500

    week0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Course{
+  constructor(col, dep, num, sec, prof, key) {
+    this.col = col;
+    this.dep = dep;
+    this.num = num;
+    this.sec = sec;
+    this.prof = prof;
+    this.key = key;
+  }
+}
+
+class App extends Component {
+  render() {
+    let courselist = [
+        new Course('CAS','CS','591','P1','CPK','0'),
+        new Course('CAS','EC','403','-','Hyung','1'),
+        new Course('CAS','EC','436','-','Newman','2')];
+
+    return (
+        <div className="App">
+          <table>
+            <thead>
+            <tr>
+              <th>college</th>
+              <th>department</th>
+              <th>number</th>
+              <th>section</th>
+              <th>professor</th>
+            </tr>
+            </thead>
+            {courselist.map(item =>
+                <tbody key = {item.key}>
+                  <tr>
+                    <td>{item.col}</td>
+                    <td>{item.dep}</td>
+                    <td>{item.num}</td>
+                    <td>{item.sec}</td>
+                    <td>{item.prof}</td>
+                  </tr>
+                </tbody>
+            )}
+          </table>
+        </div>
+    );
+  }
 }
 
 export default App;
