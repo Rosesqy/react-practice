f4ab4400f353c8265a43ea0ea52d6b1bbf9daf2e
Qingyang Shi <Rosesqy@users.noreply.github.com>
2020-02-29 22:36:09 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,42 +4,41 @@ import './App.css';
 
 class Course{
   constructor(col, dep, num, sec, prof, key) {
-    this.col = col;
-    this.dep = dep;
-    this.num = num;
-    this.sec = sec;
-    this.prof = prof;
-    this.key = key;
+      this.col = col;
+      this.dep = dep;
+      this.num = num;
+      this.sec = sec;
+      this.prof = prof;
+      this.key = `${col}${dep}${num}${sec}`;
   }
 }
 
 class App extends Component {
   render() {
     let courselist = [
-        new Course('CAS','CS','591','P1','CPK','0'),
-        new Course('CAS','EC','403','-','Hyung','1'),
-        new Course('CAS','EC','436','-','Newman','2')];
+        new Course('CAS','CS','591','P1','CPK'),
+        new Course('CAS','EC','403','-','Hyung'),
+        new Course('CAS','EC','436','-','Newman')];
+
+    const columns = ['College','Department','Number','Section','Professor'];
 
     return (
         <div className="App">
           <table>
             <thead>
             <tr>
-              <th>college</th>
-              <th>department</th>
-              <th>number</th>
-              <th>section</th>
-              <th>professor</th>
+                {columns.map(column =>
+                    <th key = {column}>{column}</th>)}
             </tr>
             </thead>
-            {courselist.map(item =>
-                <tbody key = {item.key}>
+            {courselist.map(course =>
+                <tbody key = {course.key}>
                   <tr>
-                    <td>{item.col}</td>
-                    <td>{item.dep}</td>
-                    <td>{item.num}</td>
-                    <td>{item.sec}</td>
-                    <td>{item.prof}</td>
+                    <td>{course.col}</td>
+                    <td>{course.dep}</td>
+                    <td>{course.num}</td>
+                    <td>{course.sec}</td>
+                    <td>{course.prof}</td>
                   </tr>
                 </tbody>
             )}
